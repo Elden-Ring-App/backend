874f2e51bd4f0bc0f2a3f699a29d09bcc3ba9f23
pjnp5 <paulojnpinto02@ua.pt>
2024-09-04 02:37:27 +0100

    requirements.txt updated Created utils folder to have a logger class usable everywhere Created endpoint to get an ammo by id Created ammo Model for standardization Updated main to route to the ammo service and connect with the Database on startup and disconnect on shutdown

--- db/mongo.py
+++ db/mongo.py
@@ -1,6 +1,9 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
+from utils.logger import logger
+
+from pymongo.errors import ServerSelectionTimeoutError
 
 load_dotenv()
 
@@ -17,12 +20,22 @@ async def connect_to_mongo():
     username = os.getenv("USERNAME")
     password = os.getenv("PASSWORD")
     port = os.getenv("PORT")
-    db_name = os.getenv("B_NAME")
+    db_name = os.getenv("DB_NAME")
     host = os.getenv("HOST")
 
-    mongodb.client = AsyncIOMotorClient(f"mongodb://{username}:{password}@{host}:{port}/{db_name}")
-    mongodb.db = mongodb.client[db_name]
+    try:
+        mongodb.client = AsyncIOMotorClient(f"mongodb://{username}:{password}@{host}:{port}/")
+        #mongodb.client = AsyncIOMotorClient(f"mongodb://{username}:{password}@localhost:{port}/") # to test with no Docker
+        await mongodb.client.server_info()
+        mongodb.db = mongodb.client.get_database(db_name)
+
+        logger.info("Successfully connected to MongoDB")
+    except ServerSelectionTimeoutError as e:
+        logger.error(f"Failed to connect to MongoDB: {e}")
+        raise e
 
 
 async def close_mongo_connection():
-    mongodb.client.close()
+    if mongodb.client:
+        mongodb.client.close()
+        logger.info("Closed MongoDB connection")
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from db.mongo import connect_to_mongo, close_mongo_connection, mongodb
+from api.v1.endpoints import ammos
 
 app = FastAPI()
 
+app.include_router(ammos.router, prefix="/api/v1")
 
-@app.get('/')
-async def root():
-    return {"message": "Hello World"}
+
+@app.on_event("startup")
+async def startup_db_client():
+    await connect_to_mongo()
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    await close_mongo_connection()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
